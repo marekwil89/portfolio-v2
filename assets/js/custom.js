73d0599158d81a5c7807f83240bb6a8e3e4e2260
marekwil89 <marekwil89@gmail.com>
2017-02-12 23:26:48 +0100

    arrow-down fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,16 +17,19 @@ $(document).ready(function(){
   }
 
   function anchorScroll(){
-    $('a[href*="#"]:not([href="#"])').click(function(){
+    $('a[href*="#"]:not([href="#"])').click(function(e){
     if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
 
       var target = $(this.hash);
+      if(e.currentTarget.className != 'arrow-bottom'){
+        toggleNav()
+      }
       
       target = target.length ? target:$('[name='+this.hash.slice(1)+']');
       if(target.length){$('html, body').animate({
         scrollTop:target.offset().top - 50
       },1000);
-        toggleNav()
+        
         return false;
       }}
     });
